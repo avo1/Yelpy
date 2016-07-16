7acf39caa3b50efd897ac6209596bdd8b37fde44
avo1 <voanhdung@gmail.com>
2016-07-16 22:04:36 +0700

    remove bridge file, update pod, update to swift2.2

--- Yelpy/Yelpy-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "BDBOAuth1Manager/NSDictionary+BDBOAuth1Manager.h"
-#import "BDBOAuth1Manager/BDBOAuth1RequestOperationManager.h"
-#import "AFNetworking/UIImageView+AFNetworking.h"
-#import <MBProgressHUD/MBProgressHUD.h>
-#import <BEMCheckBox/BEMCheckBox.h>
